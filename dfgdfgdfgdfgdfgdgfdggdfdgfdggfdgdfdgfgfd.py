dd2a0c1b63f72df8a4aaac56764d179fe0183449
jaggeddima <115393859+jaggeddima@users.noreply.github.com>
2023-01-29 16:57:28 +0700

    Add files via upload

---
+++ dfgdfgdfgdfgdfgdgfdggdfdgfdggfdgdfdgfgfd.py
@@ -0,0 +1,11 @@
+import time
+
+spisok = ["Боклажан","Норма","ЕЕЕЕ Бой","Молоко..."]
+
+print (spisok)
+
+spisok.append(input("Надо ещё что-то"))
+spisok.append(input("Надо ещё что-то"))
+
+print (spisok)
+
